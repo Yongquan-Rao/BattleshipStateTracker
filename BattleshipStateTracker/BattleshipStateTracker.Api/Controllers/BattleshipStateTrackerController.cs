f0b8f89b1e7d68f76222b41479aaa687dba3f393
Rao <>
2020-11-19 17:30:21 +1100

    add createboard interface

---
+++ BattleshipStateTracker/BattleshipStateTracker.Api/Controllers/BattleshipStateTrackerController.cs
@@ -0,0 +1,39 @@
+﻿using BattleshipStateTracker.Api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BattleshipStateTracker.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BattleshipStateTrackerController : ControllerBase
+    {
+        private readonly ILogger<BattleshipStateTrackerController> _logger;
+        private readonly IBattleshipBoardRepository _repo;
+
+        public BattleshipStateTrackerController(ILogger<BattleshipStateTrackerController> logger, IBattleshipBoardRepository repo)
+        {
+            _logger = logger;
+            _repo = repo;
+        }
+
+        [HttpPost("createboard")]
+        public IActionResult CreateBoard()
+        {
+            var res = _repo.CreateBoard();
+            return Ok(res);
+        }
+
+        [HttpPost]
+        public IActionResult AddBattleship()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult TakeAttack()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ BattleshipStateTracker/BattleshipStateTracker.Api/Repositories/BattleshipBoardRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BattleshipStateTracker.Api.Repositories
+{
+    public interface IBattleshipBoardRepository
+    {
+        List<List<int>> CreateBoard();
+
+        bool AddBattleship();
+
+        bool TakeAttack();
+    }
+
+    public class BattleshipBoardRepository : IBattleshipBoardRepository
+    {
+        private const int LENGTH = 10;
+        private List<List<int>> _board;
+
+        public bool AddBattleship()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<List<int>> CreateBoard()
+        {
+            _board = new List<List<int>>();
+            for (int i = 0; i < LENGTH; i++)
+            {
+                var row = new List<int>();
+                for (int j = 0; j < LENGTH; j++)
+                {
+                    row.Add(0);
+                }
+                _board.Add(row);
+            }
+
+            return _board;
+        }
+
+        public bool TakeAttack()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
